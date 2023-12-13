3b9d5cbd8e25ad761f64c72c451f9fa039743e56
KinanKazmi <kinan.kazmi@gmail.com>
2023-12-14 00:14:16 +0500

    test updated

--- __tests__/App.test.js
+++ __tests__/App.test.js
@@ -1,6 +1,27 @@
 import React from 'react';
 import { render } from '@testing-library/react-native';
+import { NativeModules } from 'react-native';
 import App from '../src/App';
+
+NativeModules.RNGestureHandlerModule = {
+  attachGestureHandler: jest.fn(),
+  createGestureHandler: jest.fn(),
+  dropGestureHandler: jest.fn(),
+  updateGestureHandler: jest.fn(),
+  forceTouchAvailable: jest.fn(),
+  State: {},
+  Directions: {}
+};
+
+NativeModules.PlatformConstants = {
+  forceTouchAvailable: false,
+};
+
+NativeModules.UIManager = {
+  RCTView: () => ({
+    directEventTypes: {},
+  }),
+};
 
 test('renders App component', () => {
   const { getByText } = render(<App />);
